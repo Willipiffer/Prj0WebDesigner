bce9bab4b8e59c40b9ce1a6db28996112ac600a2
MarcoTani <114740595+MarcoTani@users.noreply.github.com>
2022-11-04 11:03:03 +0100

    Add files via upload

--- Esercizio a Gruppi/js/script.js
+++ Esercizio a Gruppi/js/script.js
@@ -9,20 +9,22 @@ function Foto(url, titolo, prezzo, quantita, id){
     this.id = id;
 
     this.cardFoto = function(){
-        var card = '<div class="col-3"><div class="card" style="width : 18rem;"><img src="' + this.url + '" class="card-img-top"><div class=card-body"><h5 class="card-title m-3">' + this.titolo + '</h5><p class="card-text m-3">Prezzo: €' + this.prezzo + '<br>Quantità rimanente: ' + this.quantita + '</p><a href="#" class="btn btn-primary m-3" id="button' + this.id + '">Acquista</button></div></div></div>'
+        var card = '<div class="col-3"><div class="card" style="width : 18rem;"><img src="' + this.url + '" class="card-img-top"><div class=card-body"><h5 class="card-title m-3">' + this.titolo + '</h5><p class="card-text m-3">Prezzo: €' + this.prezzo + '<br>Quantità rimanente: ' + this.quantita + '</p><a href="#" class="btn btn-primary m-3" onclick="sottrai(' + this.id + ')">Acquista</button></div></div></div>'
         return card;
     }
 }
 
+/* <button onclick="myFunction()">Click me</button> */
+
 var galleria = [
-    new Foto("https://source.unsplash.com/300x300/?cat", "Foto Gatto", 5, 10, 1),
-    new Foto("https://source.unsplash.com/300x300/?dog", "Foto Cane", 7, 12, 2),
-    new Foto("https://source.unsplash.com/300x300/?sea", "Foto Mare", 10, 7, 3),
-    new Foto("https://source.unsplash.com/300x300/?flower", "Foto Fiore", 12, 4, 4),
-    new Foto("https://source.unsplash.com/300x300/?car", "Foto Macchina", 5, 5, 5),
-    new Foto("https://source.unsplash.com/300x300/?fruit", "Foto Frutta", 10, 5, 6),
-    new Foto("https://source.unsplash.com/300x300/?piano", "Foto Pianoforte", 500, 1, 7),
-    new Foto("https://source.unsplash.com/300x300/?guitar", "Foto Chitarra", 2, 50, 8)
+    new Foto("https://source.unsplash.com/300x300/?cat", "Foto Gatto", 5, 10, 0),
+    new Foto("https://source.unsplash.com/300x300/?dog", "Foto Cane", 7, 12, 1),
+    new Foto("https://source.unsplash.com/300x300/?sea", "Foto Mare", 10, 7, 2),
+    new Foto("https://source.unsplash.com/300x300/?flower", "Foto Fiore", 12, 4, 3),
+    new Foto("https://source.unsplash.com/300x300/?car", "Foto Macchina", 5, 5, 4),
+    new Foto("https://source.unsplash.com/300x300/?fruit", "Foto Frutta", 10, 5, 5),
+    new Foto("https://source.unsplash.com/300x300/?piano", "Foto Pianoforte", 500, 1, 6),
+    new Foto("https://source.unsplash.com/300x300/?guitar", "Foto Chitarra", 2, 50, 7)
 ]
 
 
@@ -41,7 +43,7 @@ function aggiungiFoto(){
     var titolo = document.getElementById("titoloImg").value;
     var prezzo = Number(document.getElementById("prezzoImg").value);
     var quantita = Number(document.getElementById("quantitaImg").value);
-    var id = galleria.length + 1;
+    var id = galleria.length;
 
     var foto = new Foto(url, titolo, prezzo, quantita, id);
     galleria.push(foto);
@@ -55,6 +57,17 @@ function aggiungiFoto(){
 
     console.log(galleria);
 }
+
+function sottrai(indice){
+    if(galleria[indice].quantita <= 0){
+        alert("Articolo momentaneamente non disponibile");
+    }else{
+    galleria[indice].quantita--
+    stampaFoto()
+    }
+}
+
+console.log(galleria[1].quantita);
 
 
 var button = document.getElementById("button");
